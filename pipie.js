8eff33eb67409e34b32f4f7cd74075b49fdd3399
Michael Underwood <mike.underwood@gmail.com>
2015-10-26 18:12:37 -0600

    Switch to spigot algorithm

--- pipie.js
+++ pipie.js
@@ -1,46 +1,68 @@
 /*
- * Based on code from the Python mailing list,
- * https://mail.python.org/pipermail/edu-sig/2012-December/010721.html
+ * Based on the spigot algorithm, with code from
+ *   http://stackoverflow.com/questions/4084571/implementing-the-spigot-algorithm-for-%CF%80-pi
  */
 function act_on_pi_digits (n, f)
 {
-    var k = 2;
-    var a = 4;
-    var b = 1;
-    var a1 = 12;
-    var b1 = 4;
-    var p, q, d, d1;
-    var atmp, btmp, a1tmp, b1tmp;
+    var len = Math.floor(10*n/3) + 1;
+    var A = [];
 
-    while (n > 0)
+    for (var i = 0; i < len; i++)
     {
-        p = k*k;
-        q = 2*k + 1;
-        k += 1;
-        atmp = a;
-        btmp = b;
-        a1tmp = a1;
-        b1tmp = b1;
-        a = a1tmp;
-        b = b1tmp;
-        a1 = p*atmp + q*a1tmp;
-        b1 = p*btmp + q*b1tmp;
-        d = a/b;
-        d1 = a1/b1;
-        while (d == d1)
+        A[i] = 2;
+    }
+
+    var nines = 0;
+    var predigit = 0;
+
+    for (var j = 1; j < n+1; ++j)
+    {
+        var q = 0;
+
+        for (var i = len; i > 0; --i)
         {
-            f(Math.floor(d)); // Pass the digit to the callback
-            n -= 1;
-            a = 10*(a%b);
-            a1 = 10*(a1%b1);
-            d = a/b;
-            d1 = a1/b1;
+            var x = Math.floor(10*A[i-1] + q*i);
+            A[i-1] = Math.floor(x % (2*i - 1));
+            q = Math.floor(x/(2*i - 1));
+        }
+
+        A[0] = q%10;
+        q = Math.floor(q / 10);
+
+        if (q == 9) {
+            ++nines;
+        }
+        else if (q == 10) {
+            f(predigit + 1);
+            for (var k = 0; k < nines; ++k)
+            {
+                f(0);
+            }
+            predigit = 0;
+            nines = 0;
+        }
+        else {
+            f(predigit);
+            predigit = q;
+            while (nines > 0) {
+                f(9);
+                nines--;
+            }
+
         }
     }
+
+    f(predigit);
+
+    // Empty the large array
+    A.length = 0;
 }
 
-function build_pie (n)
+function build_pie (n, sel_pie, sel_pct)
 {
+    var complete_pct = d3.select(sel_pct);
+    complete_pct.text('0%');
+
     var counts = [];
     var count = 0;
     for (var i = 0; i < 10; i++)
@@ -48,12 +70,65 @@ function build_pie (n)
         counts[i] = 0;
     }
 
+
+    var radius = 150;
+
+    var color = d3.scale.category10();
+
+    var svg = d3.select(sel_pie)
+        .append('svg')
+            .attr('width', 2*radius)
+            .attr('height', 2*radius)
+        .append('g')
+            .attr('transform', 'translate(' + radius + ',' + radius + ')');
+
+    // This will draw the arcs of the pie chart
+    var arc = d3.svg.arc()
+        .outerRadius(radius);
+
+    // This will calculate the angles to pass to arc
+    var pie = d3.layout.pie()
+        .value(function (d) { return d; })
+        .sort(null) // leave in their initial order
+
+    var path = svg.selectAll('path')
+        .data(pie([0,0,0,1,0,0,0,0,0,0]))
+        .enter()
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', function (d, i) {
+                return color(i);
+            })
+            .each(function (d) { this.orig = d;});
+
+    // Some of this updating and tweening is based on:
+    //   http://codepen.io/orchard/pen/bltfm
+    // Store the displayed angles in _current.
+    // Then, interpolate from _current to the new angles.
+    // During the transition, _current is updated in-place by d3.interpolate.
+    function arcTween(a) {
+        var i = d3.interpolate(this.orig, a);
+        this.orig = i(0);
+        return function(t) {
+            return arc(i(t));
+        };
+    }
+
+    ten_percent = Math.round(n / 10);
     var add_digit = function (m)
     {
         counts[m] +=1;
         count++;
-        console.log(count + ': Got m = ' + m, counts);
+        if (count % ten_percent == 0) {
+            completion = Math.round(100 * count / n) + '%'
+            complete_pct.text(completion);
+            path.data(pie(counts))
+            path.transition().duration(500).attrTween("d", arcTween); // redraw the arcs
+        }
     }
 
     act_on_pi_digits(n, add_digit);
+
+    path.data(pie(counts))
+    path.transition().duration(500).attrTween("d", arcTween); // redraw the arcs
 }
